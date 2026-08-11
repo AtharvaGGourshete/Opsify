19279789356913ce857a3028c9b1c7dd00fb2b8f
AtharvaGGourshete <goursheteatharva@gmail.com>
2026-08-12 01:47:15 +0530

    Updated index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import { ExpressAuth } from "@auth/express";
-import GitHub from "@auth/express/providers/github";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.set("trust proxy", true);
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(PORT, () => {
