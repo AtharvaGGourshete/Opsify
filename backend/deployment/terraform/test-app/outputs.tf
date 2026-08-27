cc7f48eafc49da1499449036441f6e20c5258e78
AtharvaGGourshete <goursheteatharva@gmail.com>
2026-08-27 22:30:06 +0530

    add ecs cluster terraform resource

--- backend/deployment/terraform/test-app/outputs.tf
+++ backend/deployment/terraform/test-app/outputs.tf
@@ -8,3 +8,11 @@ output "public_subnet_ids" {
     aws_subnet.public_b.id
   ]
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_cluster_arn" {
+  value = aws_ecs_cluster.main.arn
+}
