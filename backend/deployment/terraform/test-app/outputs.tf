91660cd8abc369352c23f78f06f3614bd87d342a
AtharvaGGourshete <goursheteatharva@gmail.com>
2026-08-27 22:38:02 +0530

    add ecs task execution role and logging

--- backend/deployment/terraform/test-app/outputs.tf
+++ backend/deployment/terraform/test-app/outputs.tf
@@ -16,3 +16,11 @@ output "ecs_cluster_name" {
 output "ecs_cluster_arn" {
   value = aws_ecs_cluster.main.arn
 }
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution.arn
+}
+
+output "cloudwatch_log_group_name" {
+  value = aws_cloudwatch_log_group.app.name
+}
